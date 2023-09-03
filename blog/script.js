e79a8cefffc0b3c1d71b5e6fd4ee81209633ed75
honestmk99 <mk.honest99@gmail.com>
2023-09-03 23:12:01 +0100

    modified blog script, added markdown file

--- blog/script.js
+++ blog/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const blogPostsContainer = document.getElementById('blogPosts');
     const markdownFiles = [
+      'chatgptWeakness20230828.md',
       'AIVideo20230903.md',
       // Add other Markdown files here
     ];
